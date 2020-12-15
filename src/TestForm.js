92a7a1aeb01a90b5d66edb35e4f21c9fe375221c
xh88 <rarityrings333@gmail.com>
2020-12-15 15:06:01 -0500

    added changes

--- src/TestForm.js
+++ src/TestForm.js
@@ -1,60 +1,16 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, Image, Text, View, Button, Alert } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import React from 'react';
+import { Text, View } from 'react-native';
 
-const Separator = () => (
-    <View style={styles.separator} />
-  );
-
-export default function TestForm() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <StatusBar style="auto" />
-            <Text>Welcome to the app.</Text>
-            <Text style="display: none;" />
-            <Image 
-                fadeDuration={1000}            
-                source={{ 
-                    width: 200,
-                    height: 200,
-                    uri: "https://picsum.photos/seed/picsum/200" }}/>
-            <Button onPress={() => Alert.alert("Button pressed")}
-                title="Learn More"
-                color="#841584"
-                accessibilityLabel="Learn more about this purple button"
-                />
-        </SafeAreaView>
-
-
-    )
-
+const TestForm = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Hello, world!</Text>
+    </View>
+  )
 }
-
-
-
-
-
-
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    title: {
-        textAlign: 'center',
-        marginVertical: 8,
-      },
-      fixToText: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-      },
-      separator: {
-        marginVertical: 8,
-        borderBottomColor: '#737373',
-        borderBottomWidth: StyleSheet.hairlineWidth,
-      }, 
-  });
-  
+export default TestForm;
